da1b13c33991a80973d585d9191fe3d45e4c4df8
quade <quade@aluminati.net>
2025-07-04 17:54:49 +0200

    first batch of images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,99 @@
 "use strict";
 
+
 class Situ {
 	constructor() {
 		this.elSitu = document.getElementById('situ');
 		this.elSituMdl = document.getElementById('mdl-situ');
+		this.elGalleryFeatured = document.getElementById('galleryFeatured');
+
+		this.gallery = [];
+
+		this.galleryCard = `<div class="col-4 gallery-card">
+				<div class="img">
+					<img src="./assets/images/gallery/{{fileName}}" />
+				</div>
+				<p class="title">{{title}}</p>
+				<hr />
+				<p>
+					Dimensions: {{dimensions}}
+				</p>
+				<hr />
+				<p>
+					Concept: {{concept}}
+				</p>
+				<hr />
+				<p>
+					Year: {{year}}
+				</p>
+			</div>`;
+
+		this.init();
+
+
+
 		if (this.elSitu) {
 			this.elSitu.addEventListener('click', (event) => {
 				this.showSituSelection();
 			});
 		}
 	}
+
+	async init() {
+		await this.loadGallery();
+		const featuredGalleyItems = this.featuredGalleryItems();
+		if (featuredGalleyItems.length > 0) {
+			this.loadFeaturedGalleryItems();
+		}
+	}
+
+	async loadGallery() {
+		await fetch('./assets/js/gallery.json')
+			.then(response => response.json())
+			.then(data => {
+				this.gallery = data;
+				console.log('gallery loaded:', this.gallery);
+				// You can now use this.gallery to build your gallery
+			})
+			.catch(error => {
+				console.error('Error loading gallery.json:', error);
+			});
+
+		if (this.elSitu) {
+			this.elSitu.addEventListener('click', (event) => {
+				this.showSituSelection();
+			});
+		}
+	}
+
+	featuredGalleryItems() {
+		return this.gallery.filter(item => item.featured).slice(0, 3);
+	}
+
+		craftGalleryCard(item) {
+		return `
+    <div class="col-4 gallery-card">
+      <div class="img">
+        <img src="./assets/images/gallery/${item.fileName}" alt="${item.title}" />
+      </div>
+      <p class="title">${item.title}</p>
+      <hr />
+      <p>Dimensions: ${item.dimensions || 'N/A'}</p>
+      <hr />
+      <p>Concept: ${item.concept || 'N/A'}</p>
+      <hr />
+      <p>Year: ${item.year || 'N/A'}</p>
+    </div>
+  `;
+	}
+
+	loadFeaturedGalleryItems() {
+		const items = this.featuredGalleryItems().map(item => this.craftGalleryCard(item)).join('');
+		this.elGalleryFeatured.innerHTML = items;
+	}
+
+
+
 
 	showSituSelection() {
 		console.log('hello verse');
